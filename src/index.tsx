f416eb443e541e1cc11cf1257d841b0b3dee8b35
kouji0705 <2580.hurank.0262.ichiro@gmail.com>
2022-11-06 19:15:04 +0900

    add: ServiceWorker設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
+
+// developmentの時のみ、ServiceWorkerを登録する
+if (process.env.REACT_APP_ENV === 'test') {
+  // if (process.env.REACT_APP_ENV === 'dev') {
+  const {worker} = require('./mocks/browser');
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -15,8 +20,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
